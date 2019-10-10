43a9770198491cc71da507467715ee818e980d71
Nicola Baccillieri <baccillierinicola@gmail.com>
2019-10-11 00:22:00 +0200

    Initial commit

---
+++ training/server.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import time
+import datetime
+
+HOST = '192.168.1.8'  # this is your localhost
+PORT = 8888
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# socket.socket: must use to create a socket.
+# socket.AF_INET: Address Format, Internet = IP Addresses.
+# socket.SOCK_STREAM: two-way, connection-based byte streams.
+print('socket created')
+
+# Bind socket to Host and Port
+try:
+    s.bind((HOST, PORT))
+except socket.error as err:
+    print('Bind Failed, Error Code: ' + str(err))
+    sys.exit()
+
+print('Socket Bind Success!')
+# listen(): This method sets up and start TCP listener.
+s.listen(10)
+print('Socket is now listening')
+conn, addr = s.accept()
+print('Connect with ' + addr[0] + ':' + str(addr[1]))
+data = b''
+while True:
+    buf = conn.recv(4096)
+    data = data + buf
+    if len(buf) < 4096:
+        date_string = str(datetime.datetime.now().timestamp()).replace('.', '')
+        f = open('./images/' + date_string + '.jpg', 'wb')
+        f.write(data)
+        f.close()
+        data = b''
+        conn.send(str.encode("OK\n"))
