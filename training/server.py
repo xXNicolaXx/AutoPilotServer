da89b14813ad6baab04b90b98b89e88e611088f3
Nicola Baccillieri <n.baccillieri@reply.it>
2019-10-15 14:25:06 +0200

    [ADD] csv saving

--- training/server.py
+++ training/server.py
@@ -1,10 +1,9 @@
 import socket
 import sys
 import datetime
-import time
+import csv
 
-
-HOST = '192.168.1.8'  # this is your localhost
+HOST = '192.168.0.176'  # this is your localhost
 PORT = 8888
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,25 +26,32 @@ print('Socket is now listening')
 conn, addr = s.accept()
 print('Connect with ' + addr[0] + ':' + str(addr[1]))
 data = b''
-queueArray = []
+imageArray = []
 dateArray = []
+directionArray = []
 while True:
     buf = conn.recv(8192)
 
     if buf[-3:] != b'eof':
         data = data + buf
     else:
-        data = data + buf[:-3]
-        queueArray.append(data)
+        data = data + buf[:-4]
+        direction = buf[-4:len(buf) - 3].decode('utf-8')
+        directionArray.append(direction)
+        imageArray.append(data)
         date_string = str(datetime.datetime.now().timestamp()).replace('.', '')
         dateArray.append(date_string)
         data = b''
-    if len(queueArray) >= 100:
+    if len(imageArray) >= 10:
         break
-for index, item in enumerate(queueArray):
+print("Saving collected images")
+for index, item in enumerate(imageArray):
     with open('./images/' + dateArray[index] + '.jpg', 'wb') as f:
         f.write(item)
+print("Saving data to csv")
+with open('data.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile, delimiter=',', quotechar='|')
+    for index, item in enumerate(dateArray):
+        writer.writerow(['images/' + item + '.jpg', str(directionArray[index])])
+
 print("Finish")
-
-
-
